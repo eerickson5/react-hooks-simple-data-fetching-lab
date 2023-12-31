01584f900da1453f6f31bd083fa23d46d03f8b45
Elise Erickson <elisece717@gmail.com>
2023-12-31 16:50:22 -0500

    passes tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React from "react";
+
+export default function App(){
+    const [dog, setDog] = React.useState(null)
+
+    React.useEffect( () => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then( data => {
+            setDog(data.message)
+        })
+    }, [])
+
+    if (!dog) return <p>Loading...</p>
+
+    return(
+        <img alt="A Random Dog" src={dog}/>
+    )
+}
